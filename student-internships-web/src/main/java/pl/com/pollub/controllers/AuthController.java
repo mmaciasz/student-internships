70314de2071ba50fc7b41638496a2cfdbcd10e69
Mateusz <mr.mati1@wp.pl>
2016-10-27 19:44:46 +0200

    Error handling

--- student-internships-web/src/main/java/pl/com/pollub/controllers/AuthController.java
+++ student-internships-web/src/main/java/pl/com/pollub/controllers/AuthController.java
@@ -1,7 +1,9 @@
 package pl.com.pollub.controllers;
 
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 
@@ -9,10 +11,20 @@ import java.security.Principal;
  * Created by mmaciasz on 2016-10-27.
  */
 @RestController
-public class AuthController {
+public class AuthController implements ErrorController {
 
     @RequestMapping("/user")
     public Principal user(Principal user) {
         return user;
+    }
+
+    @RequestMapping("/error")
+    public ModelAndView error() {
+        return new ModelAndView("redirect:/#/error");
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 }
